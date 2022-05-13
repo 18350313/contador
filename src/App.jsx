6ae8749560c3b9f3d8ef61388f08221cb3048c75
SC3 <cesarguizarortiz@gmail.com>
2022-05-13 11:15:28 -0500

    actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,87 @@
-import {useState} from "react";
+import { useState } from "react";
+
 function App() {
   const [cuenta, setCuenta] = useState(0);
   const [paso, setPaso] = useState(1);
 
-  const handleClickresta =() => {
-    console.log(paso);
-    setCuenta(cuenta - paso);
-  };
-  const handleResetClick =() => {
-    setCuenta(0);
-    setPaso(1);
-  };
-  const handleClicksuma =() => {
+  const handleClick = () => {
     setCuenta(cuenta + paso);
   };
-  const handleInputChange =(e) => {
-    if (isNaN(e.target.value)){
+
+  const handleClick1 = () => {
+    setCuenta(cuenta - paso);
+  };
+
+  const handleClick0 = () => {
+    setCuenta(cuenta - cuenta);
+    setPaso(1);
+  };
+  
+
+  const handleInputChange = (e) => {
+    if (isNaN(e.target.value)) {
       return;
     }
     setPaso(Number(e.target.value));
-  };
+  }
 
   return (
     <div className="App">
-    <h3>Contador</h3>
-    <hr />
-    <h2 className="text-center">0</h2>
-    <hr />
-    <div style={{display: "flex", 
-    justifyContent: "flex-end", 
-    marginRight: "5px",}}>
-      <label>Paso<input id="paso" name="paso" type="text" onChange={handleInputChange}style={{marginLeft: "5px", width:"60px"}}/></label>
-      <button type="button" className="btn btn-primary" onClick={handleClickresta} style={{marginLeft: "5px"}}>-1</button>
-      <button type="button" className="btn btn-primary" onClick={handleResetClick} style={{marginLeft: "5px"}}>RESET</button>
-      <button type="button" className="btn btn-primary" onClick={handleClicksuma} style={{marginLeft: "5px"}}>+1</button>
+      <div className="mx-auto container card m-4 p-4 shadow-lg">
+        <h1>Contador</h1>
+        <hr />
+        <h2 className="text-center">{cuenta}</h2>
+        <hr />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            marginRight: "5px",
+          }}
+        >
+          <div class="input-group me-3">
+            <span class="input-group-text" id="basic-addon1">
+                Paso
+            </span>
+            <input
+              name="paso"
+              id="paso"
+              value={paso}
+              type="text"
+              onChange={handleInputChange}
+              class="form-control"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+
+          <button
+            type="button"
+            onClick={() => {
+              handleClick();
+            }}
+            className="btn btn-outline-danger"
+          >
+            +{paso}
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick1();
+            }}
+            className="btn btn-outline-danger mx-3"
+          >
+            -{paso}
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick0();
+            }}
+            className="btn btn-outline-danger"
+          >
+            Reset
+          </button>
+        </div>
       </div>
     </div>
   );
